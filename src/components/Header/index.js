84687fb3c0005e04a1e1a757fb3a7d97e2732ebd
MedChouk <chouk.med20@gmail.com>
2022-02-12 22:17:37 +0100

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Header
+**/
+
+export const Header = (props) => {
+  return(
+    <div className="header"></div>
+   )
+
+ }
---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Header } from './../../components/Header/index';
+import { MenuHeader } from './../../components/MenuHeader/index';
+
+/**
+* @author
+* @function HomePage
+**/
+
+const HomePage = (props) => {
+  return(
+    <div>
+      <Header />
+      <MenuHeader />
+    </div>
+   )
+
+ }
+
+export default HomePage
