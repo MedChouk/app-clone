1fc73ceace35e9036c4aa4c4f1dba8a40af65ed0
MedChouk <chouk.med20@gmail.com>
2023-02-21 21:56:38 +0100

    Increase Cart Quantity

--- src/containers/CartPage/index.js
+++ src/containers/CartPage/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "./../../components/UI/Card/index";
 import Layout from "../../components/Layout";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./style.css";
-
+import { CartItem } from './CartItem/index';
+import { addToCart, } from "../../actions";
 /**
  * @author
  * @function CartPage
@@ -11,30 +12,60 @@ import "./style.css";
 
 export const CartPage = (props) => {
   const cart = useSelector((state) => state.cart);
-  const cartItems = cart.cartItems;
+  // const cartItems = cart.cartItems;
+  const [cartItems, setCartItems] = useState(cart.cartItems);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setCartItems(cart.cartItems);
+  }, [cart.cartItems]);
+
+  const onQuantityIncrement = (_id, qty) => {
+    console.log({_id, qty});
+    // console.log(cartItems[_id]);
+    const { name, price, img } = cartItems[_id];
+    dispatch(addToCart({ _id, name, price, img }, 1));
+  };
+
+  const onQuantityDecrement = (_id, qty) => {
+    console.log({_id, qty});
+    const { name, price, img } = cartItems[_id];
+    dispatch(addToCart({ _id, name, price, img }, -1));
+  };
+
 
   return (
     <Layout>
-      <div className="cartContainer">
+      <div className="cartContainer"  style={{ alignItems: "flex-start" }}>
         <Card
           headerLeft={`My Cart`}
           headerRight={<div>Deliver to</div>}
-          style={{ width: "calc(100% - 400px)", overflow: "hidden" }}
+          
         >
           {/* {JSON.stringify(cartItems)} */}
-          {Object.keys(cartItems).map((key, index) => (
-            <div key={index} className="flexRow">
-              <div className="cartProductContainer">
-                <img src="" />
-              </div>
-              <div className="cartItemDetails">
-                <div>{cartItems[key].name} - qty - {cartItems[key].qty}</div>
-                <div>Delivery in 3 - 5 days</div>
-              </div>
-            </div>
-          ))}
+          {Object.keys(cartItems).map((key, index) => 
+            <CartItem 
+              key={index}
+              cartItem={cartItems[key]}
+              onQuantityInc={onQuantityIncrement}
+              onQuantityDec={onQuantityDecrement}
+            />
+            // <div key={index} className="flexRow">
+            //   <div className="cartProductContainer">
+            //     <img src="" />
+            //   </div>
+            //   <div className="cartItemDetails">
+            //     <div>{cartItems[key].name} - qty - {cartItems[key].qty}</div>
+            //     <div>Delivery in 3 - 5 days</div>
+            //   </div>
+            // </div>
+          )}
         </Card>
-        <Card style={{ width: "500px" }}>Price</Card>
+        <Card
+        headerLeft='Price'
+        style={{ width: "500px" }}>
+          
+        </Card>
       </div>
     </Layout>
   );
